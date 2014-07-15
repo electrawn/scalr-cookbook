833cb8f129be25ccde953dd6e80b9692de03301f
electrawn <electrawn@yahoo.com>
2014-07-15 15:06:03 -0500

    Add dependencies

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'Apache License v2.0'
 description      'Library to access the Scalr agent in Chef (szradm)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.5'
+
+
+depends "nokogiri"
+depends "active_support"
