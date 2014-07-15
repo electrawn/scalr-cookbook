8bd85b0b72f31cd0159b2196a85def7c938d1321
electrawn <electrawn@yahoo.com>
2014-07-15 16:42:15 -0500

    Try Nori.

--- libraries/scalr_roles.rb
+++ libraries/scalr_roles.rb
@@ -5,19 +5,21 @@ include Chef::Mixin::ShellOut
 
 module Scalr
   def self.roles()
-    require 'nokogiri'
-	require 'active_support/core_ext/hash/conversions'
-  
+    #require 'nokogiri'
+	#require 'active_support/core_ext/hash/conversions'
+    require 'nori'
+	 
     # Retrieve Global Roles
     # We use szradm and not environment variables so that we can run
     # in a standalone chef-client run
     p = Chef::Mixin::ShellOut.shell_out '/usr/local/bin/szradm',  '-q', 'list-global-variables'
     gv_response = p.stdout
-    gv_doc = Nokogiri::XML(gv_response)
+    #gv_doc = Nokogiri::XML(gv_response)
 
     # Parse and return Roles	
 	
-    roles = Hash.from_xml(gv_doc.to_s)
+    #roles = Hash.from_xml(gv_doc.to_s)
+	roles = Nori.new.parse(gv_response)
     
     roles
   end
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,11 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
-node.set['xml']['compiletime'] = true
-include_recipe 'xml::default'
-node.set['build-essential']['compile_time'] = true
-include_recipe 'build-essential::default'
+#node.set['xml']['compiletime'] = true
+#include_recipe 'xml::default'
+#node.set['build-essential']['compile_time'] = true
+#include_recipe 'build-essential::default'
 
-chef_gem "activesupport" do
-   version "4.1.4"
-end
+chef_gem "nori"
+
+#chef_gem "activesupport" do
+#   version "4.1.4"
+#end
