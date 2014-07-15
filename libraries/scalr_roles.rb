17924e94a8878debae1da79b8f731ef8a6ac2c9d
electrawn <electrawn@yahoo.com>
2014-07-15 15:23:34 -0500

    move requires

--- libraries/scalr_roles.rb
+++ libraries/scalr_roles.rb
@@ -1,13 +1,12 @@
-require 'nokogiri'
-require 'active_support/core_ext/hash/conversions'
+
 require 'chef/mixin/shell_out'
 include Chef::Mixin::ShellOut
-chef_gem 'nokogiri'
-chef_gem 'active_support'
+
 
 module Scalr
   def self.roles()
-
+    require 'nokogiri'
+	require 'active_support/core_ext/hash/conversions'
   
     # Retrieve Global Roles
     # We use szradm and not environment variables so that we can run
@@ -16,7 +15,8 @@ module Scalr
     gv_response = p.stdout
     gv_doc = Nokogiri::XML(gv_response)
 
-    # Parse and return Roles
+    # Parse and return Roles	
+	
     roles = Hash.from_xml(gv_doc.to_s)
     
     roles
