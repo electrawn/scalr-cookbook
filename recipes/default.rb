bf2d7aa776d32172164e1b1555030bf6e4cc79ee
electrawn <electrawn@yahoo.com>
2014-07-15 16:33:56 -0500

    add build-essential

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,8 @@
 
 node.set['xml']['compiletime'] = true
 include_recipe 'xml::default'
+node.set['build-essential']['compile_time'] = true
+include_recipe 'build-essential::default'
 
 chef_gem "activesupport" do
    version "4.1.4"
