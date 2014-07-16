9f9b8a237ed591856cc7ab2c2d2846056e928c7f
electrawn <electrawn@yahoo.com>
2014-07-16 14:11:18 -0500

    get farm role params changed to list

--- libraries/scalr_roles.rb
+++ libraries/scalr_roles.rb
@@ -5,8 +5,6 @@ include Chef::Mixin::ShellOut
 
 module Scalr
   def self.roles()
-    #require 'nokogiri'
-	#require 'active_support/core_ext/hash/conversions'
     require 'nori'
 	require "rexml/document"
 	
@@ -14,16 +12,36 @@ module Scalr
     # We use szradm and not environment variables so that we can run
     # in a standalone chef-client run
     p = Chef::Mixin::ShellOut.shell_out '/usr/local/bin/szradm',  '-q', 'list-roles'
+	#Strip out the tabs, newlines and other garbage 
     gv_response = p.stdout.lines.map{|line| line = line.strip}.join
-    #gv_doc = Nokogiri::XML(gv_response)
-
-    # Parse and return Roles	
-	
-    #roles = Hash.from_xml(gv_doc.to_s)
-	roles = Nori.new(:parser => :rexml).parse(gv_response)
+	#Strip out response tag.
+	gv_response = gv_response.gsub '<response>', ''
+	gv_response = gv_response.gsub '</response>', ''
+    
+    # Parse and return Roles		
+    roles = Nori.new(:parser => :rexml).parse(gv_response)
     
     roles
   end
+  
+  def self.list_farm_role_params(farm_role_id)
+    require 'nori'
+	require "rexml/document"
+	
+    # Retrieve Global Roles
+    # We use szradm and not environment variables so that we can run
+    # in a standalone chef-client run
+    p = Chef::Mixin::ShellOut.shell_out '/usr/local/bin/szradm',  '-q', 'list-farm-role-params', "farm-role-id=#{farm_role_id}"
+	#Strip out the tabs, newlines and other garbage 
+    gv_response = p.stdout.lines.map{|line| line = line.strip}.join
+	#Strip out response tag.
+	gv_response = gv_response.gsub '<response>', ''
+	gv_response = gv_response.gsub '</response>', ''
+    
+    # Parse and return Roles		
+    list_farm_role_params = Nori.new(:parser => :rexml).parse(gv_response)
+    
+    list_farm_role_params
 end
 
 
